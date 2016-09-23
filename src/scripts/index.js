176f620c1dd1b9c8559c6b84f5f39ced6f0ae96f
Jisoo Park <jso9603@gmail.com>
2016-09-23 11:33:07 +0900

    edit scripts about jQuery load function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//브라우저가 DOM트리 생성한 직후
+$(document).ready(function () {
+  console.log("$(document).ready()");
+});
 
+//윈도우가 로딩된 후
+$(window).on("load", function () {
+  console.log("$(window).on('load')");
+});
 
+//웹페이지의 로딩이 끝나는 시점에 실행되는 함수
+window.onload = function () {
+  console.log("window.onload");
+};
